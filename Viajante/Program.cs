7b0bd56019abc10605b63679ddc84ca28f070a97
Nathan R.F.P <nathanseribeiro@hotmail.com>
2019-03-15 08:50:29 -0300

    Testando novos métodos de calculo de caminho

--- Viajante/Program.cs
+++ Viajante/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Viajante
 {
     class Program
-    {        
+    {
         static void Main(string[] args)
         {
+
             Console.Write("Numero de cidades: ");
             int cidades = int.Parse(Console.ReadLine());
 
             int[,] distancias = new int[cidades, cidades];
-
 
         }
 
@@ -21,13 +22,41 @@ namespace Viajante
             return false;
         }
 
-        static string ForcaButa(int[,] distancias)
+        static string ForcaBruta(int[,] distancias)
         {
-            Caminho caminho = new Caminho(distancias.GetLength(0));
+            List<Caminho> caminhos = new List<Caminho>();
+            int j = 0;
+            int numCidades = distancias.GetLength(0);
 
+            while (j < numCidades)
+            {
+                Caminho caminhoatual = new Caminho(numCidades + 1);
 
+                for (int i = 1; i < numCidades; i++)
+                {
+                    caminhoatual.caminho[j] = distancias[];
+                }
+
+                caminhos.Add(caminhoatual);
+                j++;
+            }            
+            
+            
 
             return null;
+        }
+
+        static void GeradorDeCaminho(int[,] pontos)
+        {
+            int size = pontos.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    
+                }
+            }
         }
     }
 }
