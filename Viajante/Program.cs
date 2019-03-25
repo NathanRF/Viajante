3ac2b0f8d69587a589ff5f3ff89e28f833873c67
Nathan Ribeiro <42881774+NathanRF@users.noreply.github.com>
2019-03-25 11:30:00 -0300

    Finalisados testes

--- Viajante/Program.cs
+++ Viajante/Program.cs
@@ -12,51 +12,71 @@ namespace Viajante
             Console.WriteLine("Calculando...");
 
             ForcaBruta a3 = new ForcaBruta(3);
-            Console.WriteLine("Força bruta 3 calculado");
+            Console.WriteLine("\nForça bruta com 3 cidades calculado: "+a3);
             ForcaBruta a4 = new ForcaBruta(4);
-            Console.WriteLine("Força bruta 4 calculado");
+            Console.WriteLine("\nForça bruta com 4 cidades calculado: "+a4);
             ForcaBruta a5 = new ForcaBruta(5);
-            Console.WriteLine("Força bruta 5 calculado");
+            Console.WriteLine("\nForça bruta com 5 cidades calculado: "+a5);
             ForcaBruta a6 = new ForcaBruta(6);
-            Console.WriteLine("Força bruta 6 calculado");
+            Console.WriteLine("\nForça bruta com 6 cidades calculado: "+a6);
             ForcaBruta a7 = new ForcaBruta(7);
-            Console.WriteLine("Força bruta 7 calculado");
+            Console.WriteLine("\nForça bruta com 7 cidades calculado: "+a7);
             ForcaBruta a8 = new ForcaBruta(8);
-            Console.WriteLine("Força bruta 8 calculado");
+            Console.WriteLine("\nForça bruta com 8 cidades calculado: "+a8);
             ForcaBruta a9 = new ForcaBruta(9);
-            Console.WriteLine("Força bruta 9 calculado");
+            Console.WriteLine("\nForça bruta com 9 cidades calculado: "+a9);
             ForcaBruta a10 = new ForcaBruta(10);
-            Console.WriteLine("Força bruta 10 calculado");
+            Console.WriteLine("\nForça bruta com 10 cidades calculado: "+a10);
             //ForcaBruta a15 = new ForcaBruta(15);
             //ForcaBruta a20 = new ForcaBruta(20);
             //ForcaBruta a25 = new ForcaBruta(25);
             //ForcaBruta a30 = new ForcaBruta(30);
 
             ForcaBrutaOtimizacao b3 = new ForcaBrutaOtimizacao(3);
+            Console.WriteLine("\nForça bruta otimizada com 3 cidades calculado: " + b3);
             ForcaBrutaOtimizacao b4 = new ForcaBrutaOtimizacao(4);
+            Console.WriteLine("\nForça bruta otimizada com 4 cidades calculado: " + b4);
             ForcaBrutaOtimizacao b5 = new ForcaBrutaOtimizacao(5);
+            Console.WriteLine("\nForça bruta otimizada com 5 cidades calculado: " + b5);
             ForcaBrutaOtimizacao b6 = new ForcaBrutaOtimizacao(6);
+            Console.WriteLine("\nForça bruta otimizada com 6 cidades calculado: " + b6);
             ForcaBrutaOtimizacao b7 = new ForcaBrutaOtimizacao(7);
+            Console.WriteLine("\nForça bruta otimizada com 7 cidades calculado: " + b7);
             ForcaBrutaOtimizacao b8 = new ForcaBrutaOtimizacao(8);
+            Console.WriteLine("\nForça bruta otimizada com 8 cidades calculado: " + b8);
             ForcaBrutaOtimizacao b9 = new ForcaBrutaOtimizacao(9);
+            Console.WriteLine("\nForça bruta otimizada com 9 cidades calculado: " + b9);
             ForcaBrutaOtimizacao b10 = new ForcaBrutaOtimizacao(10);
-            ForcaBrutaOtimizacao b15 = new ForcaBrutaOtimizacao(15);
-            ForcaBrutaOtimizacao b20 = new ForcaBrutaOtimizacao(20);
-            ForcaBrutaOtimizacao b25 = new ForcaBrutaOtimizacao(25);
-            ForcaBrutaOtimizacao b30 = new ForcaBrutaOtimizacao(30);
+            Console.WriteLine("\nForça bruta otimizada com 10 cidades calculado: " + b10);
+            //ForcaBrutaOtimizacao b15 = new ForcaBrutaOtimizacao(15);
+            //ForcaBrutaOtimizacao b20 = new ForcaBrutaOtimizacao(20);
+            //ForcaBrutaOtimizacao b25 = new ForcaBrutaOtimizacao(25);
+            //ForcaBrutaOtimizacao b30 = new ForcaBrutaOtimizacao(30);
 
             AlgoritmoOn2 c3 = new AlgoritmoOn2(3);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 3 cidades calculado: " + c3);
             AlgoritmoOn2 c4 = new AlgoritmoOn2(4);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 4 cidades calculado: " + c4);
             AlgoritmoOn2 c5 = new AlgoritmoOn2(5);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 5 cidades calculado: " + c5);
             AlgoritmoOn2 c6 = new AlgoritmoOn2(6);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 6 cidades calculado: " + c6);
             AlgoritmoOn2 c7 = new AlgoritmoOn2(7);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 7 cidades calculado: " + c7);
             AlgoritmoOn2 c8 = new AlgoritmoOn2(8);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 8 cidades calculado: " + c8);
             AlgoritmoOn2 c9 = new AlgoritmoOn2(9);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 9 cidades calculado: " + c9);
             AlgoritmoOn2 c10 = new AlgoritmoOn2(10);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 10 cidades calculado: " + c10);
             AlgoritmoOn2 c15 = new AlgoritmoOn2(15);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 15 cidades calculado: " + c15);
             AlgoritmoOn2 c20 = new AlgoritmoOn2(20);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 20 cidades calculado: " + c20);
             AlgoritmoOn2 c25 = new AlgoritmoOn2(25);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 25 cidades calculado: " + c25);
             AlgoritmoOn2 c30 = new AlgoritmoOn2(30);
+            Console.WriteLine("\nAlgoritmo O(n^2) com 30 cidades calculado: " + c30);
 
             using (StreamWriter csv = new StreamWriter("Resultados.csv"))
             {
@@ -70,21 +90,17 @@ namespace Viajante
                 csv.WriteLine(string.Format("8;{0};{1};{2}", a8.tempoGasto, b3.tempoGasto, c3.tempoGasto));
                 csv.WriteLine(string.Format("9;{0};{1};{2}", a9.tempoGasto, b3.tempoGasto, c3.tempoGasto));
                 csv.WriteLine(string.Format("10;{0};{1};{2}", a10.tempoGasto, b3.tempoGasto, c3.tempoGasto));
-                csv.WriteLine(string.Format("15;{0};{1};{2}", /*a15.tempoGasto,*/"Não testado", b3.tempoGasto, c3.tempoGasto));
-                csv.WriteLine(string.Format("20;{0};{1};{2}",/* a20.tempoGasto,*/"Não testado", b3.tempoGasto, c3.tempoGasto));
-                csv.WriteLine(string.Format("25;{0};{1};{2}",/* a25.tempoGasto,*/"Não Testado", b3.tempoGasto, c3.tempoGasto));
-                csv.WriteLine(string.Format("30;{0};{1};{2}",/* a30.tempoGasto,*/"Não testado", b3.tempoGasto, c3.tempoGasto));
+                csv.WriteLine(string.Format("15;{0};{1};{2}", /*a15.tempoGasto,*/"Nao testado", /*b3.tempoGasto,*/"Nao testado", c3.tempoGasto));
+                csv.WriteLine(string.Format("20;{0};{1};{2}",/* a20.tempoGasto,*/"Nao testado", /*b3.tempoGasto,*/"Nao testado", c3.tempoGasto));
+                csv.WriteLine(string.Format("25;{0};{1};{2}",/* a25.tempoGasto,*/"Nao testado", /*b3.tempoGasto,*/"Nao testado", c3.tempoGasto));
+                csv.WriteLine(string.Format("30;{0};{1};{2}",/* a30.tempoGasto,*/"Nao testado", /*b3.tempoGasto,*/"Nao testado", c3.tempoGasto));
+            }            
 
-            }
-
-            ForcaBrutaOtimizacao test = new ForcaBrutaOtimizacao(4);
-            Console.WriteLine(test);
-
-            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
             Console.ReadKey();
             Console.Clear();
 
-            Console.WriteLine("\nResultados salvos no arquivo \"Resultados.csv\"");
+            Console.WriteLine("Resultados salvos no arquivo \"Resultados.csv\"");
             Console.Write("Pressione qualquer tecla para continuar...");
             Console.ReadKey();
         }
